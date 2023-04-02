7254d79dc591431f0ae67ea1c00b9b824d83aa31
kr3pt0n <egor20042013@gmail.com>
2023-04-02 19:36:02 +0200

    lesson 3

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import reactDom from 'react-dom';
+import { StyleSheet, Text, SafeAreaView, Button, Alert, Image, TouchableWithoutFeedback } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
+  const HandleTextPress = () => console.log('textpressed');
+  const HandleButtonPress = () => Alert.alert("title", "main message", [
+    {text: "ok"},
+    {text: "cancel"},
+  ]);
+  const HandleButtonPress2 = () => Alert.prompt("title", "main message", text => console.log(text));
+
+    return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text} onPress={HandleTextPress}>Hello</Text>
+      <Button title ={'button1'}  onPress={HandleButtonPress}/>
+      <Button title ={'button2'}  onPress={HandleButtonPress2}/>
+      <TouchableWithoutFeedback onPress={HandleButtonPress}>
+      <Image source={{
+        height: 200,
+        uri: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'
+      }}/>
+      </TouchableWithoutFeedback>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: 
+  { 
+    marginTop: 50,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text:
+  {
+    fontWeight: 'bold',
+    fontSize: 40,
+    color: 'red',
   },
 });
